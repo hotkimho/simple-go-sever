09daad134a81c2fe54becca9836e7280be2e2754
hotkimho <rlaghekd777@naver.com>
2024-10-23 18:35:20 +0900

    chore: 웹훅 테스트

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 		logLevel = "info(default value)"
 	}
 
-	fmt.Println("trigger test webhook")
+	fmt.Println("trigger test webhook2")
 	fmt.Printf("PORT: %s\n", port)
 	fmt.Printf("CORS_ORIGIN: %s\n", corsOrigin)
 	fmt.Printf("DEBUG_MODE: %s\n", debugMode)
